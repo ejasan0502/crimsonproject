ea05667838168676b90d5964ed090aefabdec6e7
Jesse <scooby0988@sbcglobal.net>
2013-02-07 21:18:26 -0800

    Added skill limits based on level and skill points

--- Crimson/Assets/Scripts/SkillsWindow.cs
+++ Crimson/Assets/Scripts/SkillsWindow.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 // creates an interactive skills menu that can be used to pick new skills when leveling
+// Works by placing script in Skills Component of Player(FPS Controller)
 public class SkillsWindow : MonoBehaviour 
 {
 	private GUIStyle menuStyle;
@@ -13,6 +14,7 @@ public class SkillsWindow : MonoBehaviour
 	public Rect r;
 	
 	int numSkills;
+	int playerLvl;
 	string charClass;
 	bool windowOpen = false;
 	
@@ -21,6 +23,9 @@ public class SkillsWindow : MonoBehaviour
 			Frenzy, TargetVitals, EmpShot;
 	bool ToolMaster, RobotMaster, SummonTurret, RobotDefence, SummonAssault, 
 			SelfDestruct, RepairRobots, SummonMAADD;
+	bool[] skillsInUse;
+	
+	int numSkillsInUse;
 	
 	
 	void Awake ()
@@ -41,10 +46,13 @@ public class SkillsWindow : MonoBehaviour
 		menuStyle.normal.textColor = Color.white;
 		menuStyle.font = (Font)Resources.Load ("Fonts/After_Shok");
 		
-		// FIX TO BE BASED ON PLAYER
-		numSkills = 5;
+		skillsInUse = new bool[8];
+		
+		// FIX TO BE BASED ON PLAYER LEVEL playerLvl = player.level;
+		playerLvl = 3;
+		numSkills = playerLvl;
 		charClass = "Engineer";
-		Debug.Log(charClass);
+		Debug.Log(player.level);
 	}
 	
 	void Update()
@@ -91,100 +99,244 @@ public class SkillsWindow : MonoBehaviour
 		
 		// create a clickable buttons
 		//Pistol Mastery
+		// Skill Number: 0
+		// Requirement: None
 		PistolMaster = GUI.Toggle (new Rect(r.width/8, 40, r.width/4, 40), PistolMaster, new GUIContent("Pistol Mastery", 
 				"Increases the damage done by Pistol weapons."));
 		if (PistolMaster)
 		{
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills)
+			{
+				skillsInUse[0] = true;
+			}
+			else
+				PistolMaster = !PistolMaster;
 		}
 		else if (!PistolMaster)
 		{
+			skillsInUse[0] = false;	
 		}
 		//Rifle Mastery
+		// Skill Number: 1
+		// Requirement: None
 		RifleMaster = GUI.Toggle (new Rect((r.width/8)*3, 40, r.width/4, 40), RifleMaster, new GUIContent("Rifle Mastery", 
 				"Increases the damage done by Rifle weapons."));
 		if (RifleMaster)
 		{
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills)
+			{
+				skillsInUse[1] = true;
+			}
+			else 
+				RifleMaster = !RifleMaster;
 		}
 		else if (!RifleMaster)
 		{
+			skillsInUse[1] = false;
 		}
 		//Burning Shot
+		// Skill Number: 2
+		// Requirement: None
 		BurningShot = GUI.Toggle (new Rect((r.width/8)*5, 40, r.width/4, 40), BurningShot, new GUIContent("Burning Shot", 
 				"Shoots a bullet that burns the target over time."));
 		if (BurningShot)
 		{
-			BurningShot script;
-			script = skillsScript.GetComponent<BurningShot>();
-			script.enabled = true;
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills)
+			{
+				BurningShot script;
+				script = skillsScript.GetComponent<BurningShot>();
+				script.enabled = true;
+				skillsInUse[2] = true;
+			}
+			else 
+				BurningShot = !BurningShot;
 		}
 		else if (!BurningShot)
 		{
 			BurningShot script;
 			script = skillsScript.GetComponent<BurningShot>();
-			script.enabled = true;
+			script.enabled = false;
+			skillsInUse[2] = true;
 		}
 		//Quick Shot
+		// Skill Number: 3
+		// Requirement: Level 3
 		QuickShot = GUI.Toggle (new Rect(r.width/4, 80, r.width/4, 40), QuickShot, new GUIContent("Quick Shot", 
 				"Increases how fast the player shoots for a short time."));
 		if (QuickShot)
 		{
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 3)
+			{
+				skillsInUse[3] = true;
+			}
+			else
+				QuickShot = !QuickShot;
 		}
 		else if (!QuickShot)
 		{
+			skillsInUse[3] = false;
 		}
 		//Poison Shot
+		// Skill Number: 4
+		// Requirement: Level 3
 		PoisonShot = GUI.Toggle (new Rect((r.width/8)*5, 80, r.width/4, 40), PoisonShot, new GUIContent("Poison Shot", 
 				"Shoots a bullet that does medium poison damage for a short time."));
 		if (PoisonShot)
 		{
-			PoisonShot script;
-			script = skillsScript.GetComponent<PoisonShot>();
-			script.enabled = true;
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 3)
+			{
+				PoisonShot script;
+				script = skillsScript.GetComponent<PoisonShot>();
+				script.enabled = true;
+				skillsInUse[4] = true;	
+			}
+			else
+				PoisonShot = !PoisonShot;
 		}
 		else if (!PoisonShot)
 		{
 			PoisonShot script;
 			script = skillsScript.GetComponent<PoisonShot>();
-			script.enabled = true;
+			script.enabled = false;
+			skillsInUse[4] = false;
 		}
 		//Frenzy
+		// Skill Number: 5
+		// Requirement: Level 7
 		Frenzy = GUI.Toggle (new Rect(r.width/4, 130, r.width/4, 40), Frenzy, new GUIContent("Frenzy", 
 				"Increases the damage done player weapons for a short time."));
 		if (Frenzy)
 		{
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 7)
+			{
+				skillsInUse[5] = true;
+			}
+			else 
+				Frenzy = !Frenzy;
 		}
 		else if (!Frenzy)
 		{
+			skillsInUse[5] = false;
 		}
 		//Target Vitals
+		// Skill Number: 6
+		// Requirement: Level 7
 		TargetVitals = GUI.Toggle (new Rect((r.width/8)*5, 130, r.width/4, 40), TargetVitals, new GUIContent("Target Vitals", 
 				"Shoots a bullet that causes high bleed damage for a short time."));
 		if (TargetVitals)
 		{
-			TargetVitals script;
-			script = skillsScript.GetComponent<TargetVitals>();
-			script.enabled = true;
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 7)
+			{
+				TargetVitals script;
+				script = skillsScript.GetComponent<TargetVitals>();
+				script.enabled = true;
+				skillsInUse[6] = true;
+			}
+			else
+				TargetVitals = !TargetVitals;
 		}
 		else if (!TargetVitals)
 		{
 			TargetVitals script;
 			script = skillsScript.GetComponent<TargetVitals>();
-			script.enabled = true;
+			script.enabled = false;
+			skillsInUse[6] = false;
 		}
 		//EMP Shot
+		// Skill Number: 7
+		// Requirement: Level 10
 		EmpShot = GUI.Toggle (new Rect((r.width/8)*3, 180, r.width/4, 40), EmpShot, new GUIContent("EMP Shot", 
 				"Shoots an EMP that does extra damage to robots."));
 		if (EmpShot)
 		{
-			EMPShot script;
-			script = skillsScript.GetComponent<EMPShot>();
-			script.enabled = true;
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 10)
+			{
+				EMPShot script;
+				script = skillsScript.GetComponent<EMPShot>();
+				script.enabled = true;
+				skillsInUse[7] = true;
+			}
+			else
+				EmpShot = !EmpShot;
 		}
 		else if (!EmpShot)
 		{
 			EMPShot script;
 			script = skillsScript.GetComponent<EMPShot>();
-			script.enabled = true;
+			script.enabled = false;
+			skillsInUse[7] = false;
 		}
 		
 		Debug.Log("Marksman window");
@@ -217,8 +369,8 @@ public class SkillsWindow : MonoBehaviour
 		GUI.DragWindow (rec);
 		
 		// show tooltip
-		GUI.Label(new Rect((r.width/2)-50, 600, 80, 50), "Description");
-		GUI.Label(new Rect(75, 625,r.width, 50), GUI.tooltip);
+		GUI.Label(new Rect((r.width/2)-50, (r.height/10)*9, 80, 50), "Description");
+		GUI.Label(new Rect((r.width/15), (r.height/15)*14,r.width, 50), GUI.tooltip);
 	}
 	
 	void engineerWindow (int windowID)
@@ -230,107 +382,253 @@ public class SkillsWindow : MonoBehaviour
 		
 		// create a clickable buttons
 		//Tool Mastery
+		// Skill Number: 0
+		// Requirement: None
 		ToolMaster = GUI.Toggle (new Rect(r.width/8, 40, r.width/4, 40), ToolMaster, new GUIContent("Tool Mastery", 
 				"Increases the damage done by Tool weapons."));
 		if (ToolMaster)
 		{
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills)
+			{
+				skillsInUse[0] = true;
+			}
+			else 
+				ToolMaster = !ToolMaster;
+		
 		}
 		else if (!ToolMaster)
 		{
+			skillsInUse[0] = false;
+			
 		}
 		//Robot Mastery
+		// Skill Number: 1
+		// Requirement: None
 		RobotMaster = GUI.Toggle (new Rect((r.width/8)*3, 40, r.width/4, 40), RobotMaster, new GUIContent("Robot Mastery", 
 				"Increases the damage done by Robots."));
 		if (RobotMaster)
 		{
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills)
+			{
+				skillsInUse[1] = true;
+			}
+			else
+				RobotMaster = !RobotMaster;
 		}
 		else if (!RobotMaster)
 		{
+			skillsInUse[1] = false;
 		}
 		//Summon Turret
+		// Skill Number: 2
+		// Requirement: None
 		SummonTurret = GUI.Toggle (new Rect((r.width/8)*5, 40, r.width/4, 40), SummonTurret, new GUIContent("Summon Turret", 
 				"Summons a turret to help the player for a short time."));
 		if (SummonTurret)
 		{
-			SummonTurret script;
-			script = skillsScript.GetComponent<SummonTurret>();
-			script.enabled = true;
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills)
+			{
+				SummonTurret script;
+				script = skillsScript.GetComponent<SummonTurret>();
+				script.enabled = true;
+				skillsInUse[2] = true;
+			}
+			else 
+				SummonTurret = !SummonTurret;
 		}
 		else if (!SummonTurret) 
 		{
 			SummonTurret script;
 			script = skillsScript.GetComponent<SummonTurret>();
 			script.enabled = false;
+			skillsInUse[2] = false;
 		}
 		//Robot Defence
+		// Skill Number: 3
+		// Requirement: Level 3
 		RobotDefence = GUI.Toggle (new Rect(r.width/4, 80, r.width/4, 40), RobotDefence, new GUIContent("Robot Defence", 
 				"Increases the defence of the player when they have a robot summoned."));
 		if (RobotDefence)
 		{
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 3)
+			{
+				skillsInUse[3] = true;
+			}
+			else
+				RobotDefence = !RobotDefence;
 		}
 		else if (!RobotDefence)
 		{
+			skillsInUse[3] = false;
 		}
 		//Summon Assault
+		// Skill Number: 4
+		// Requirement: Level 3
 		SummonAssault = GUI.Toggle (new Rect((r.width/8)*5, 80, r.width/4, 40), SummonAssault, new GUIContent("Summon Assault Bot", 
 				"Summons a assault robot to help the player for a short time."));
 		if (SummonAssault)
 		{
-			SummonAssault script;
-			script = skillsScript.GetComponent<SummonAssault>();
-			script.enabled = true;
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 3)
+			{
+				SummonAssault script;
+				script = skillsScript.GetComponent<SummonAssault>();
+				script.enabled = true;
+				skillsInUse[4] = true;
+			}
+			else 
+				SummonAssault = !SummonAssault;
 		}
 		else if (!SummonAssault)
 		{
 			SummonAssault script;
 			script = skillsScript.GetComponent<SummonAssault>();
 			script.enabled = false;
+			skillsInUse[4] = false;
 		}
 		//Self Destruct
+		// Skill Number: 5
+		// Requirement: Level 7
 		SelfDestruct = GUI.Toggle (new Rect(r.width/4, 130, r.width/4, 40), SelfDestruct, new GUIContent("Self Destruct", 
-				"Causes the furthest player owned robot to self destruct, doing damage to nearby enemies."));
+				"The furthest player robot self destructs, damaging nearby enemies."));
 		if (SelfDestruct)
 		{
-			SelfDestruct script;
-			script = skillsScript.GetComponent<SelfDestruct>();
-			script.enabled = true;
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 7)
+			{
+				SelfDestruct script;
+				script = skillsScript.GetComponent<SelfDestruct>();
+				script.enabled = true;
+				skillsInUse[5] = true;
+			}
+			else 
+				SelfDestruct = !SelfDestruct;
 		}
 		else if (!SelfDestruct)
 		{
 			SelfDestruct script;
 			script = skillsScript.GetComponent<SelfDestruct>();
 			script.enabled = false;
+			skillsInUse[5] = false;
 		}
 		//Repair Robots
+		// Skill Number: 6
+		// Requirement: Level 7
 		RepairRobots = GUI.Toggle (new Rect((r.width/8)*5, 130, r.width/4, 40), RepairRobots, new GUIContent("Repair Robots", 
 				"Repairs all robots in range."));
 		if (RepairRobots)
 		{
-			Repair script;
-			script = skillsScript.GetComponent<Repair>();
-			script.enabled = true;
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 7)
+			{
+				Repair script;
+				script = skillsScript.GetComponent<Repair>();
+				script.enabled = true;
+				skillsInUse[6] = true;
+			}
+			else
+				RepairRobots = !RepairRobots;
 		}
 		else if (!RepairRobots)
 		{
 			Repair script;
 			script = skillsScript.GetComponent<Repair>();
 			script.enabled = false;
+			skillsInUse[6] = false;
 		}
 		//Summon MAADD
+		// Skill Number: 7
+		// Requirement: Level 10
 		SummonMAADD = GUI.Toggle (new Rect((r.width/8)*3, 180, r.width/4, 40), SummonMAADD, new GUIContent("Summon M.A.A.D.D.", 
 				"Summon Mobile Anti-Alien Destruction Device for a short time."));
 		if (SummonMAADD)
 		{
+			//  check if player has avail skill points
+			numSkillsInUse = 0;
+			for (int i=0; i<skillsInUse.Length; i++)
+			{
+				if (skillsInUse[i] == true)
+				{
+					numSkillsInUse++;
+				}
+			}
+			if (numSkillsInUse <= numSkills && playerLvl >= 10)
+			{
+				skillsInUse[7] = true;
+			}
+			else
+				SummonMAADD = !SummonMAADD;
 		}
 		else if (!SummonMAADD)
 		{
+			skillsInUse[7] = false;
 		}
 		
 		Debug.Log("Engineer window");
 		// Skills
 		scrollPosition = GUI.BeginScrollView (new Rect (0, r.height * 0.1f, r.width, r.height),
 														scrollPosition, new Rect (0, 0, r.width, r.height));
-			
+		
 		int amount = 0;
 		ArrayList skills = new ArrayList();
 		if (player.charClass == "Marksman")
@@ -352,11 +650,11 @@ public class SkillsWindow : MonoBehaviour
 		GUI.EndScrollView ();
 		
 		Rect rec = new Rect (0, 0, r.width, r.height * 0.1f);
-
+		 
 		GUI.DragWindow (rec);
 		
 		// show tooltip
-		GUI.Label(new Rect((r.width/2)-50, 600, 80, 50), "Description");
-		GUI.Label(new Rect(75, 625,r.width, 50), GUI.tooltip);
+		GUI.Label(new Rect((r.width/2)-50, (r.height/10)*9, 80, 50), "Description");
+		GUI.Label(new Rect((r.width/15), (r.height/15)*14,r.width, 50), GUI.tooltip);
 	}
 }
